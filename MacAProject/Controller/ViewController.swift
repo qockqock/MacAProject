d368403e5886447f51688cc9fd61f88a3db30d48
김광현 <okok0840@naver.com>
2024-07-04 13:46:59 +0900

    로고 + 카테고리 commit

--- MacAProject/Controller/ViewController.swift
+++ MacAProject/Controller/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  MacAProject
-//
-//  Created by 머성이 on 7/2/24.
-//
+// 뷰컨
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let coffeeListView = CategoryView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setupCoffeeListView()
     }
-
-
+    
+    private func setupCoffeeListView() {
+        coffeeListView.delegate = self
+        view.addSubview(coffeeListView)
+        coffeeListView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+        }
+    }
 }
 
+extension ViewController: CoffeeListViewDelegate {
+    func segmentValueChanged(to index: Int) {
+        coffeeListView.collectionView.reloadData()
+    }
+    
+    func configureCollectionViewConstraints() {
+        // Additional constraints can be added here if needed
+    }
+    
+    func configureCollectionViewAppearance() {
+        coffeeListView.collectionView.backgroundColor = .white
+    }
+}
