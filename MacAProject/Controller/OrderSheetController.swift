2e2174adbcf39e84b1e8db367ed04a464aca2cbb
Deuk Ryeong <164954344+DeukRyoeng@users.noreply.github.com>
2024-07-04 18:03:29 +0900

    주석 추가

--- MacAProject/Controller/OrderSheetController.swift
+++ MacAProject/Controller/OrderSheetController.swift
@@ -9,16 +9,15 @@ import UIKit
 import SwiftUI
 import SnapKit
 
-
-
-//MakeCell 클래스: 테이블뷰셀 클래스
+//MARK: - MakeCell 클래스: 테이블뷰셀 클래스
 class MakeCell: UITableViewCell {
     let contentLabel = UILabel()
     
-    //셀의 초기화 메서드
+    // 셀의 초기화 메서드
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        // contentLabel을 contentView에 추가하고 오토레이아웃 설정
         contentView.addSubview(contentLabel)
         contentLabel.snp.makeConstraints {
             $0.edges.equalToSuperview().inset(10)
@@ -29,48 +28,51 @@ class MakeCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //셀에 들어갈 내용
+    // 셀에 들어갈 내용 구성 메서드
     func configure(){
         contentLabel.text = "Sample Text"
     }
 }
 
-
-//TableViewController 클래스
+//MARK: - TableViewController 클래스: 테이블뷰를 관리하는 클래스
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    //테이블뷰 인스턴스 생성
+    // 테이블뷰 인스턴스 생성
     private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 데이터 소스와 델리게이트 설정
         tableView.dataSource = self
         tableView.delegate = self
         
-        tableView.rowHeight = 100 //셀 하나하나의 높이
+        // 셀 하나하나의 높이 설정
+        tableView.rowHeight = 100
         
-        //cell을 재사용하기 위해 재사용 식별자를 테이블뷰에 등록
+        // 셀을 재사용하기 위해 재사용 식별자를 테이블뷰에 등록
         tableView.register(MakeCell.self, forCellReuseIdentifier: "cellID")
         
-        //테이블뷰 오토레이아웃 설정 메서드 호출
+        // 테이블뷰 오토레이아웃 설정 메서드 호출
         setupTableviewConstraints()
-        paymentButton()
         
+        // 결제 버튼 생성 메서드 호출
+        paymentButton()
     }
     
-    //모달 안에 있는 결제하기 버튼
+    //MARK: - 모달 내부에 있는 결제 Btn 함수
     func paymentButton(){
         let orderListButton = UIButton()
         
+        // 버튼의 타이틀, 색상, 배경색, 폰트 설정
         orderListButton.setTitle("결제하기", for: .normal)
         orderListButton.setTitleColor(.white, for: .normal)
         orderListButton.backgroundColor = #colorLiteral(red: 0.2734747827, green: 0.1341301203, blue: 0.003133529332, alpha: 1)
         orderListButton.titleLabel?.font = .boldSystemFont(ofSize: 24)
         orderListButton.layer.cornerRadius = 15
         
+        // 버튼을 뷰에 추가하고 오토레이아웃 설정
         self.view.addSubview(orderListButton)
-        
         orderListButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(30)
             $0.height.equalTo(65)
@@ -78,23 +80,26 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
             $0.centerX.equalToSuperview()
         }
         
+        // 버튼 클릭 시 paymentSuccessAlert 메서드를 호출하도록 설정
         orderListButton.addTarget(self, action: #selector(paymentSuccessAlert), for: .touchDown)
     }
     
+    //MARK: - 결제 성공 알림 메서드 Alert
     @objc
-    func paymentSuccessAlert() {
+      func paymentSuccessAlert() {
         let alert = UIAlertController(title: "결제 성공", message: "주문이 완료되었습니다.", preferredStyle: .alert)
-        
-        
-        let closeAction = UIAlertAction(title: "닫기", style: .default, handler: nil)
-        
+    //    let closeAction = UIAlertAction(title: "닫기", style: .default, handler: nil)
+          let closeAction = UIAlertAction(title: "닫기", style: .default) { [self] (action) in
+          // 현재 화면을 닫는 동작을 여기서 실행
+          self.dismiss(animated: false, completion: nil)
+              // 모달뷰 디스미스
+        }
         alert.addAction(closeAction)
-        
         //alert창 띄우기
         self.present(alert, animated: true, completion: nil)
-    }
+      }
     
-    //테이블뷰 오토레이아웃 설정
+    // 테이블뷰 오토레이아웃 설정
     func setupTableviewConstraints(){
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
@@ -102,43 +107,42 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
     }
     
-    
-    
+    // 테이블뷰의 섹션당 셀의 개수 설정
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return cartArray.count  //아직 값을 받아 오지 않았기 때문에 일단 주석 -> 리턴값은 테이블뷰에 전달
-        return 7
+        return 7 // 실제 데이터로 변경 필요
     }
     
+    // 셀의 내용을 설정하는 메서드
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 셀을 재사용하기 위해 큐에서 꺼내서 캐스팅
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as? MakeCell else {   //MakeCell을 타입 캐스팅
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as? MakeCell else {
             return UITableViewCell()
         }
         
-        cell.configure() // 셀을 구성하는 메서드 호출
-        cell.selectionStyle = .none // 셀 선택 시 색상 변하지 않게 설정
+        // 셀 구성 및 선택 스타일 설정
+        cell.configure()
+        cell.selectionStyle = .none
         
         return cell
     }
-    
 }
 
-
-//ViewController 역할
+//MARK: - OrderSheetController 클래스: 주문 내역 버튼을 관리하는 클래스
 class OrderSheetController: UIViewController {
-    
-    //홈에 있는 주문하기 버튼
+    //tvc 클래스 변수로 변환
+    let tvc = TableViewController()
+    // 홈에 있는 주문하기 버튼
     func paymentButton(){
         let orderListButton = UIButton()
         
+        // 버튼의 타이틀, 색상, 배경색, 폰트 설정
         orderListButton.setTitle("주문 내역", for: .normal)
         orderListButton.setTitleColor(.white, for: .normal)
         orderListButton.backgroundColor = #colorLiteral(red: 0.2734747827, green: 0.1341301203, blue: 0.003133529332, alpha: 1)
         orderListButton.titleLabel?.font = .boldSystemFont(ofSize: 24)
         orderListButton.layer.cornerRadius = 15
         
+        // 버튼을 뷰에 추가하고 오토레이아웃 설정
         self.view.addSubview(orderListButton)
-        
         orderListButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(30)
             $0.height.equalTo(65)
@@ -146,47 +150,33 @@ class OrderSheetController: UIViewController {
             $0.centerX.equalToSuperview()
         }
 
-        
+        // 버튼 클릭 시 ShowOderList 메서드를 호출하도록 설정
         orderListButton.addTarget(self, action: #selector(ShowOderList), for: .touchDown)
-        
-        
     }
     
+    //MARK: - 주문 내역 모달을 표시하는 메서드
     @objc
-    func ShowOderList(){
-        let tvc = TableViewController()
+    func ShowOderList(isShow: Bool) {
+    //MARK: - 모달 생성, 설정
         
         if let orderSheet = tvc.sheetPresentationController {
             orderSheet.detents = [.medium()]
             orderSheet.preferredCornerRadius = 20
-            /// 상단 -- 부여 잡는 부분 보이게 할것인지!
-            orderSheet.prefersGrabberVisible = true
+            orderSheet.prefersGrabberVisible = isShow
         }
-        self.present(tvc, animated: true)
+        self.present(tvc, animated: false)
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         paymentButton()
-        
     }
-    
-    
 }
 
-
-
-
-
-
-
-
+// SwiftUI 미리보기 설정
 struct PreView2: PreviewProvider {
     static var previews: some View {
         OrderSheetController().toPreview()
-        
     }
 }
 #if DEBUG
