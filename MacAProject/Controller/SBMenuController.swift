7bee8df585fd5de89417f57375b2c5fd22d0589f
김광현 <okok0840@naver.com>
2024-07-05 14:33:21 +0900

    카테고리 밑줄 추가 및 배경 삭제

--- MacAProject/Controller/SBMenuController.swift
+++ MacAProject/Controller/SBMenuController.swift
@@ -1,11 +1,3 @@
-//
-// MenuController.swift
-// MacAProject
-//
-// Created by 머성이 on 7/3/24.
-//
-
-
 import UIKit
 import SnapKit
 import SwiftUI
@@ -25,7 +17,14 @@ class SBMenuController: UIViewController {
     let categories = ["추천메뉴", "커피", "디저트", "스무디", "티", "왜먹어?"]
     
     // 카테고리 메뉴 배열
-    var drinks: [[CoffeeList]] = [CoffeeList.recommended_Menu, CoffeeList.coffee_Menu, CoffeeList.dessert_Menu, CoffeeList.smoothie_Menu, CoffeeList.tea_Menu, CoffeeList.do_not_eat_Menu]
+    var drinks: [[CoffeeList]] = [
+        CoffeeList.recommended_Menu,
+        CoffeeList.coffee_Menu,
+        CoffeeList.dessert_Menu,
+        CoffeeList.smoothie_Menu,
+        CoffeeList.tea_Menu,
+        CoffeeList.do_not_eat_Menu
+    ]
     
     var currentCategoryIndex: Int = 0
     
@@ -47,17 +46,29 @@ class SBMenuController: UIViewController {
         control.selectedSegmentTintColor = .clear
         control.apportionsSegmentWidthsByContent = true
         
+        // 카테고리 세그먼트 배경 하얗게
+        control.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        control.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        
         let normalTextAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14)
+            .font: UIFont.systemFont(ofSize: 16)
         ]
         control.setTitleTextAttributes(normalTextAttributes, for: .normal)
         
         let selectedTextAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.boldSystemFont(ofSize: 14)
+            .font: UIFont.boldSystemFont(ofSize: 16)
         ]
         control.setTitleTextAttributes(selectedTextAttributes, for: .selected)
         
         return control
+    }()
+    
+    // 선택된 세그먼트의 밑줄 View
+    let underlineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override func viewDidLoad() {
@@ -68,29 +79,40 @@ class SBMenuController: UIViewController {
         collectionView.delegate = self
         
         setupLayout()
+        moveUnderline(to: 0) // 초기 위치로 밑줄 이동
     }
     
     // 레이아웃 설정
     private func setupLayout() {
-        [logoImageView, segmentControl, collectionView].forEach {
+        [logoImageView, segmentControl, collectionView, underlineView].forEach {
             view.addSubview($0)
         }
         
-        //Logo Constraints
+        // Logo Constraints
         logoImageView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(-10) //솔비 offset 값 변경
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(-10)
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(255) //솔비 변경
+            $0.width.equalTo(255)
             $0.height.equalTo(100)
         }
         
-        //카테고리 Constraints
+        // 세그먼트 Constraints
         segmentControl.snp.makeConstraints {
-            $0.top.equalTo(logoImageView.snp.bottom).offset(5) //솔비 offset 값 변경
-            $0.left.right.equalToSuperview().inset(20)
+            $0.top.equalTo(logoImageView.snp.bottom).offset(5)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(30)
+            $0.centerX.equalToSuperview()
         }
         
-        //컬렉션뷰(메뉴 리스트) Constraints
+        // 밑줄 Constraints
+        underlineView.snp.makeConstraints {
+            $0.height.equalTo(2)
+            $0.leading.equalToSuperview().offset(10) // 초기 위치
+            $0.bottom.equalTo(segmentControl.snp.bottom).offset(2) // 세그먼트 아래에 위치
+            $0.width.equalTo(segmentControl.snp.width).dividedBy(CGFloat(categories.count))
+        }
+        
+        // 컬렉션뷰(메뉴 리스트) Constraints
         collectionView.snp.makeConstraints {
             $0.top.equalTo(segmentControl.snp.bottom).offset(30)
             $0.leading.trailing.bottom.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 20, bottom: 30, right: 20))
@@ -101,6 +123,20 @@ class SBMenuController: UIViewController {
     @objc private func segmentValueChanged(_ sender: UISegmentedControl) {
         currentCategoryIndex = sender.selectedSegmentIndex
         collectionView.reloadData()
+        moveUnderline(to: sender.selectedSegmentIndex)
+    }
+    
+    // 밑줄 이동 메서드
+    private func moveUnderline(to index: Int) {
+        let segmentWidth = segmentControl.frame.width / CGFloat(categories.count)
+        let leadingConstraint = segmentWidth * CGFloat(index) + 10 // 10은 여유로운 여백
+        
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.snp.updateConstraints {
+                $0.leading.equalToSuperview().offset(leadingConstraint)
+            }
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
@@ -121,21 +157,17 @@ extension SBMenuController: UICollectionViewDataSource, UICollectionViewDelegate
 
 extension SBMenuController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        //셀 크기 조정
         return CGSize(width: 117, height: 180)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        //셀 위 아래 간격
         return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        //셀 좌 우 간격
         return 0
     }
 }
-
 
 struct PreView123: PreviewProvider {
     static var previews: some View {
