5f9148111a79d43b04272557d284754be9a8e48d
Deuk Ryeong <164954344+DeukRyoeng@users.noreply.github.com>
2024-07-08 03:10:01 +0900

    하단 버튼 에니메이션 추가

--- MacAProject/Controller/OrderSheetController.swift
+++ MacAProject/Controller/OrderSheetController.swift
@@ -12,6 +12,7 @@ import SnapKit
 class OrderSheetController: UIViewController {
     //tvc 클래스 변수로 변환
     let tvc = BasketViewController()
+    let orderListButton = UIButton()
     
     var showModal = false
     
@@ -25,7 +26,6 @@ class OrderSheetController: UIViewController {
     
     // 홈에 있는 주문하기 버튼
     func paymentButton_Home() {
-        let orderListButton = UIButton()
         
         print("called - PaymentButton")
         // 버튼의 타이틀, 색상, 배경색, 폰트 설정
@@ -55,34 +55,54 @@ class OrderSheetController: UIViewController {
     
     // 알림 옵저버 추가
     func addNotiObserver() {
-        NotificationCenter.default.addObserver(self, selector: #selector(self.didReceiveNoti(_:)), name: NSNotification.Name("notiData"), object: nil)
+        NotificationCenter.default.addObserver(self,selector:#selector(showOrderButton(_:)), name: NSNotification.Name("notiData"),object: nil)
     }
     
-    // 알림을 받았을 때 호출되는 메서드
-    @objc
-    func didReceiveNoti(_ notification: Notification) {
-        if let showModal = notification.userInfo?["showModal"] as? Bool {
-            self.showModal = showModal
-        }
-    }
     
     // 모달 창 띄우기
     @objc
     func showOrderListModal() {
-        
+        hideOrderButton()
         if tvc.orders.isEmpty {
-            print("아직 선택된 상품 없음.")
+            print("상품 없음.")
             if let sheetViewController = tvc.sheetPresentationController {
                 sheetViewController.detents = [.medium()]
                 sheetViewController.preferredCornerRadius = 20
             }
             self.present(tvc, animated: true, completion: nil)
         } else {
+            print("상품 있음.")
             if let sheetViewController = tvc.sheetPresentationController {
                 sheetViewController.detents = [.medium()]
                 sheetViewController.preferredCornerRadius = 20
             }
             self.present(tvc, animated: true, completion: nil)
+        }
+    }
+    //버튼 숨기기 에니메이션
+    func hideOrderButton() {
+        UIView.animate(withDuration: 0.5) {
+            // 버튼을 화면 아래로 이동
+            self.orderListButton.snp.updateConstraints {
+                $0.centerX.equalToSuperview()
+                $0.bottom.equalToSuperview().inset(-40) // 화면 바깥으로 이동
+            }
+            self.view.layoutIfNeeded() // 제약 조건 업데이트
+        }
+    }
+    
+    @objc
+    func showOrderButton(_ notification: Notification) {
+        if let showModal = notification.userInfo?["showModal"] as? Bool {
+            self.showModal = showModal
+            print("showModal: \(showModal)")
+        }
+        UIView.animate(withDuration: 0.5) {
+            // 버튼을 초기 위치로 되돌림
+            self.orderListButton.snp.updateConstraints {
+                $0.bottom.equalToSuperview().inset(60) // 초기 위치로 되돌림
+            }
+            self.view.layoutIfNeeded() // 제약 조건 업데이트
         }
     }
 }
